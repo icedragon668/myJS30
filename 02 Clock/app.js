56a2084e108d2ec97b57fa7ffb2e446bf55a2dbd
Kori <icedragon668@gmail.com>
2019-04-16 23:05:53 -0400

    hour/min functional, w/ minor movement, second styling

--- 02 Clock/app.js
+++ 02 Clock/app.js
@@ -1,11 +1,25 @@
-const secondHand = document.querySelector('.second-hand')
+const secondHand = document.querySelector('.second-hand');
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
 
-setDate = () => {
-    const now = new Date()
-    const seconds = now.getSeconds()
-    const secondsDegrees = ((seconds * 6)+90)
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+setDate = function () {
+    const now = new Date();
     
-}
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hours = now.getHours();
 
-setInterval(setDate, 1000)
+    const secondsDegrees = (seconds *6) + 90;
+    const minsDegrees = (mins * 6) + (seconds/10) + 90;
+    const hoursDegrees = (hours * 15) + (mins/2) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+    console.log(seconds, secondsDegrees)
+};
+
+setInterval(setDate, 1000);
+
+setDate();
